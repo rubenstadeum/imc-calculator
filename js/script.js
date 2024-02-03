5cf736f1299b6a74622f0f42d2b2690f1139a704
Rubens Tadeu <rubenstadeumoura@gmail.com>
2024-02-03 03:10:59 -0300

    Lógica da Validação de Dados

--- js/script.js
+++ js/script.js
@@ -10,12 +10,23 @@ form.onsubmit = event => {
 
     const weight = inputWeight.value
     const height = inputHeight.value
+
+    const showAlertError = notANumber(weight) || notANumber(height)
+
+    if (showAlertError) {
+        console.log('mostrar o alerta de erro')
+        return; 
+    }
     
     const result = IMC(weight, height)
     const message = `Seu IMC é de ${result}`
 
     Modal.message.innerText = message
     Modal.open()
+}
+
+function notANumber(value) {
+    return isNaN(value) || value == ""
 }
 
 function IMC (weight, height) {
